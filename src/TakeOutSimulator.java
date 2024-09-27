1b66e7dd5e69d953dcfd86291701f682cde53259
SLYearwood <slyearwood7@gmail.com>
2024-09-27 00:54:25 -0400

    Add functionality to check user input

--- src/TakeOutSimulator.java
+++ src/TakeOutSimulator.java
@@ -1,4 +1,32 @@
 import java.util.Scanner;
 
 public class TakeOutSimulator {
+
+    private Customer customer;
+    private FoodMenu menu;
+    private Scanner input;
+
+    public TakeOutSimulator (Customer customer, FoodMenu menu, Scanner input) {
+        this.customer = customer;
+        this.menu = menu;
+        this.input = input;
+    }
+
+    private <T> T getOutputOnIntInput(String userInputPrompt, IntUserInputRetriever<T> intUserInputRetriever) {
+
+        while (true) {
+            System.out.println(userInputPrompt);
+            try {
+                if (input.hasNextInt()) {
+                    return intUserInputRetriever.produceOutputOnIntUserInput(input.nextInt());
+                } else {
+                    System.out.println("Input needs to be an 'int' type.");
+                    input.next();
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(input + " is not a valid input. Try Again!");
+            }
+        }
+    }
 }
+
